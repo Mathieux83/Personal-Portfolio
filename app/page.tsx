8632c592a3900ccdec0aff0a5d9327060fa72bd9
Mathieux83 <car.math@live.fr>
2025-08-22 14:10:56 +0200

    add statistics

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,13 @@ import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data/index";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function Home() {
 	return (
 		<div className='relative bg-black-100 flex justify-center items-center overflow-clip flex-col mx-auto sm:px-10 px-5'>
 			<div className='max-w-7xl w-full'>
+				<Analytics />
 				<FloatingNav navItems={navItems} />
 				<Hero />
 				<Grid />
